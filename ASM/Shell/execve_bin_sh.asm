c8176066aa45cd1049dc61bd9e1b0d0c5abdc0ee
PyR3Q <61708525+pyr3q@users.noreply.github.com>
2020-04-17 00:55:53 +0200

    Create execve_bin_sh.asm

---
+++ ASM/Shell/execve_bin_sh.asm
@@ -0,0 +1,19 @@
+; asm arch=x86-64
+; asm script to get a shell with execve()
+; @PyR3Q
+
+section .text
+
+global _start
+
+_start:
+	xor rsi, rsi
+	push rsi
+	mov rdi, "/bin/sh" ; string to execute the shell in sh
+	push rdi ; push rdi register in the stack
+	push rsp
+	pop rdi ; mov "/bin/sh" in rsp register
+	mov rax, 59 ; call execve() function -> 0x3b = 59 in decimal
+	push rax
+	cdq ; convert doubleword to quadword
+	syscall
